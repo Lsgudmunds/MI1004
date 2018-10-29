dbe27d925aef8430e1e7d97739f641db5c35b311
Henyxyz <henrik.a.bergfeldt@gmail.com>
2018-10-29 14:37:19 +0100

    Now extracts article source name from data. Also limited the number of headlines to 5.

--- news.py
+++ news.py
@@ -4,14 +4,16 @@ import json
 apiKey = "607faf4e6e1f4f868b184da044721073" #API KEY
 userCountry = "us" #User country, this should be dynamically genereated by user input.
 newsApi = NewsApiClient(api_key=apiKey) #Creates an instance of object NewsApiClient
-topHeadlines = newsApi.get_top_headlines(country = userCountry) #Gets top headlines from users country
+topHeadlines = newsApi.get_top_headlines(country = userCountry, page_size = 5) #Gets top headlines from users country
 dataDump = json.dumps(topHeadlines, indent=4, sort_keys=True) #Creates a string of the json dictonary. This is only for test, should be removed later.
 
 for entry in topHeadlines["articles"]: #Prints every title and url for entries in articles.
     print(entry["title"])
+    print(entry["source"]["name"])
     print(entry["url"])
     print()
 
 #print(dataDump)
 
+print("Top headlines provided by newsapi.org")
 input("Press enter to end program....")
