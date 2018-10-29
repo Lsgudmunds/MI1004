58b44a0228a6b743c3b48812b55e41a6be46eede
Henyxyz <henrik.a.bergfeldt@gmail.com>
2018-10-29 11:22:23 +0100

    git commit

--- numberinfoAPI.py
+++ numberinfoAPI.py
@@ -1,6 +1,7 @@
 import json
 import urllib3
 
+hej = "hej"
 url = "http://numbersapi.com/"
 number = input("Enter a number...")
 http = urllib3.PoolManager()
@@ -8,4 +9,5 @@ numberTrivia = http.request('GET', url+"/"+number+"/trivia")
 numberTriviaDecoded = numberTrivia.data.decode('UTF-8')
 
 print(numberTriviaDecoded)
+print(hej)
 input('Press enter to continue...')
