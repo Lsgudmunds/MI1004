e79dc6b6a8149fc4cd430700f5ce890f15da8ec6
Henyxyz <henrik.a.bergfeldt@gmail.com>
2018-10-29 14:03:13 +0100

    Updated news.py. Formated the python dictionary and extracted relevant data to the user. Added comments.

--- news.py
+++ news.py
@@ -1,11 +1,17 @@
 from newsapi import NewsApiClient
 import json
 
-apiKey = "607faf4e6e1f4f868b184da044721073"
-userCountry = "us"
-newsApi = NewsApiClient(api_key="607faf4e6e1f4f868b184da044721073")
-topHeadlines = newsApi.get_top_headlines(sources = "bbc-news")
-data = json.dumps(topHeadlines)
+apiKey = "607faf4e6e1f4f868b184da044721073" #API KEY
+userCountry = "us" #User country, this should be dynamically genereated by user input.
+newsApi = NewsApiClient(api_key=apiKey) #Creates an instance of object NewsApiClient
+topHeadlines = newsApi.get_top_headlines(country = userCountry) #Gets top headlines from users country
+dataDump = json.dumps(topHeadlines, indent=4, sort_keys=True) #Creates a string of the json dictonary. This is only for test, should be removed later.
 
-print(data)
+for entry in topHeadlines["articles"]: #Prints every title and url for entries in articles.
+    print(entry["title"])
+    print(entry["url"])
+    print()
+
+#print(dataDump)
+
 input("Press enter to end program....")
